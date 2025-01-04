3670d8a78ced9143fe36d2725445636facde5b04
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-01-04 10:12:39 +0600

    add data with js fetch

--- js/fetch.js
+++ js/fetch.js
@@ -29,13 +29,78 @@ function loadTable(){
 loadTable(); // Load Student Record on Page opening
 
 // Open Add new student Modal Box 
+function addNewModal(){
+	var addModal = document.getElementById("addModal");
+	addModal.style.display = 'block';
 
+	fetch('php/fetch-class-field.php')
+	.then((response) => response.json())
+	.then((data)=>{
+			var select = document.getElementById('classlist');
+			var option = '<option value="0" disabled selected>Select Class</option>';
+			for(var i=0; i < data.length; i++){
+				option += `<option value="${data[i].cid}">${data[i].class_name}</option>`;
+			}
+			select.innerHTML = option;
+	})
+	.catch((error) => {
+		show_message('error',"Can't Fetch Class List");
+	});
+}
 
 // Hide Modal Box / Popup Box
+function hide_modal(){
+	var addModal = document.getElementById("addModal");
+	addModal.style.display = 'none';
 
+	var editModal = document.getElementById("modal");
+	editModal.style.display = 'none';
+}
 
 // Add Student Record
+function submit_data(){
+	var fname = document.getElementById('fname').value;
+	var lname = document.getElementById('lname').value;
+	var sClass = document.getElementById('classlist').value;
+	var city = document.getElementById('city').value;
 
+	if(fname === '' || lname === '' || sClass === '0' || city === ''){
+		alert('Please Fill All The Fields');
+		return false;
+	}else{
+		var formdata = {
+			'fname' : fname,
+			'lname' : lname,
+			'class' : sClass,
+			'city' : city
+		}
+
+		jsondata = JSON.stringify(formdata);
+
+		fetch('php/fetch-insert.php',{
+			method : 'POST',
+			body : jsondata,
+			headers: {
+				'Content-type' : 'application/json',
+			}
+		})
+		.then((response) => response.json())
+		.then((result)=>{
+				if(result.insert == 'success'){
+					show_message('success','Data Inserted Successfully.');
+					loadTable();
+					hide_modal();
+					document.getElementById('addModal-form').reset();
+				}else{
+					show_message('error',"Data Can't Inserted.");
+					hide_modal();
+				}
+		})
+		.catch((error) => {
+			show_message('error',"Data not Inserted.");
+		});
+	}
+}
 
 // Open Update Modal Box and show Student record in it.
 
